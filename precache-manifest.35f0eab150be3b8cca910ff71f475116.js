46a973cdc869c72fd5b95a1133a632034931a188
Kasia Grabowska <kattie.grab@gmail.com>
2019-01-01 22:20:42 +0100

    Updates

---
+++ precache-manifest.35f0eab150be3b8cca910ff71f475116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "39dd978b763c0dd132f26cd7634aa23a",
+    "url": "/raspberry/static/media/bakery.39dd978b.ttf"
+  },
+  {
+    "revision": "d16b5eb23972a42769cd2ce7200642cc",
+    "url": "/raspberry/static/media/aleo.d16b5eb2.ttf"
+  },
+  {
+    "revision": "0ecf6ac98119902de0e009d36eed475f",
+    "url": "/raspberry/static/media/Bg_banner.0ecf6ac9.png"
+  },
+  {
+    "revision": "d0272cec7d841f062cd6",
+    "url": "/raspberry/static/js/runtime~main.d0272cec.js"
+  },
+  {
+    "revision": "94ad7a954c3a764960b9",
+    "url": "/raspberry/static/js/main.94ad7a95.chunk.js"
+  },
+  {
+    "revision": "f8eecd707a02563b3533",
+    "url": "/raspberry/static/js/1.f8eecd70.chunk.js"
+  },
+  {
+    "revision": "61e7ecb7607cd42192623d8846031b5d",
+    "url": "/raspberry/index.html"
+  }
+];
